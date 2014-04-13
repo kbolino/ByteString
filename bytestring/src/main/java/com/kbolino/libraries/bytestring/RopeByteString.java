a7c41b30e4312a3b010293440f5321b498c14801
Kristian Bolino <kbolino@kbolino.com>
2014-04-12 23:38:26 -0400

    Implement RopeByteString.subString

--- bytestring/src/main/java/com/kbolino/libraries/bytestring/RopeByteString.java
+++ bytestring/src/main/java/com/kbolino/libraries/bytestring/RopeByteString.java
@@ -38,7 +38,12 @@ final class RopeByteString extends AbstractByteString {
 		this.length = length;
 	}
 	
-	private int findString(final int index) {
+	/**
+	 * Which string is holding a byte?
+	 * @param index  The index of the byte.
+	 * @return  The index of the string in {@code this.strings}.
+	 */
+	private int whichString(final int index) {
 		final int i = Arrays.binarySearch(offsets, index);
 		if (i < 0) {
 			return -i - 2;
@@ -55,8 +60,35 @@ final class RopeByteString extends AbstractByteString {
 	/** {@inheritDoc} */
 	public byte at(int index) throws IllegalArgumentException, IndexOutOfBoundsException {
 		checkAt(index);
-		int s = findString(index);
+		int s = whichString(index);
 		return strings[s].at(index - offsets[s]);
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public ByteString subString(int beginIndex, int endIndex)
+			throws IllegalArgumentException, IndexOutOfBoundsException {
+		checkSubString(beginIndex, endIndex);
+		if (endIndex == beginIndex) {
+			return Utils.EMPTY_STRING;
+		}
+		final int first = whichString(beginIndex);
+		final int last = whichString(endIndex - 1);
+		if (first == last) {
+			final int offset = offsets[first];
+			return strings[first].subString(beginIndex - offset, endIndex - offset);
+		} else {
+			final ByteString newFirst = strings[first].subString(beginIndex - offsets[first]);
+			final ByteString newLast = strings[last].subString(0, endIndex - offsets[last]);
+			final int numStrings = last - first + 1;
+			final ByteString[] newStrings = new ByteString[numStrings];
+			newStrings[0] = newFirst;
+			newStrings[numStrings - 1] = newLast;
+			for (int i = 1; i < numStrings - 1; i++) {
+				newStrings[i] = strings[i + first];
+			}
+			return new RopeByteString(newStrings);
+		}
 	}
 
 	/** {@inheritDoc} */
@@ -73,11 +105,12 @@ final class RopeByteString extends AbstractByteString {
 		return length;
 	}
 	
+	/** {@inheritDoc} */
 	@Override
 	public int indexOf(int value, int fromIndex)
 			throws IllegalArgumentException, IndexOutOfBoundsException {
 		checkIndexOf(value, fromIndex);
-		final int s = findString(fromIndex);
+		final int s = whichString(fromIndex);
 		for (int i = s; i < strings.length; i++) {
 			final ByteString string = strings[i];
 			final int f = Math.max(fromIndex - offsets[i], 0);
